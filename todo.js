4bd88b438d1826cf69ee1b03b14557cdefa936a8
MihranG <91gasparyan@gmail.com>
2018-01-17 19:22:25 +0400

    commit with frontend imerative

--- todo.js
+++ todo.js
@@ -7,7 +7,7 @@ const expressValidator = require('express-validator');
 const app = express();
 
 let todos = [{
-    id:1,
+    id:0,
     action: 'test action'
 }];
 let todosId = todos.length;
@@ -16,18 +16,29 @@ let todosId = todos.length;
 app.use(cookieParser());
 app.use(bodyParser());
 app.use(expressValidator());
+app.use(bodyParser.json());
 
 app.set('view engine','pug');
+app.use(express.static(__dirname + '/public'));
+
 
 
 app.get ('/',(req,res)=>{
     
-    console.log('pug is rendered');
-    res.render ('todo',{todos});
+    console.log('html is rendered');
+    res.sendFile(__dirname +'/index.html');
    
 
 
 });
+
+app.get ('/api/todos', (req,res)=>{
+
+    console.log('api/todos API ');
+    res.send({todos});
+
+
+})
 
 app.post('/', (req,res)=>{
     
@@ -35,7 +46,9 @@ app.post('/', (req,res)=>{
     req.checkBody('action').notEmpty().withMessage('you need to write an action');
     const errors = req.validationErrors();
     if (errors) {
-        res.render('todo', {errors});
+        errors.forEach(error=>{
+            res.send(error.msg);
+        })
     } else {
 
         const action = req.body.action;
@@ -51,7 +64,7 @@ app.post('/', (req,res)=>{
 
         console.log(todos);
         
-        res.render('todo',{todos,});
+        res.redirect('/');
 
     }
     res.end();
@@ -59,6 +72,32 @@ app.post('/', (req,res)=>{
 
 });
 
+
+app.post('/api/todos', (req,res)=>{
+    
+
+    
+
+        const action = req.body.action;
+        
+        let id = todosId;
+
+        todos.push({
+            'id' : id, 
+            'action' : action,
+        }); 
+
+        todosId++;
+
+        console.log(todos);
+        
+        res.redirect('/');
+
+    
+    res.end();
+
+
+});
 
 
 
@@ -72,37 +111,75 @@ app.get('/edit/:id', (req, res) => {
 });
 
 
-app.post('/edit/:id', (req,res)=>{
+app.put('/api/edit', (req,res)=>{
     
 
     
         //res.render('todo');
+
     
 
-        const editableTodo = req.params.id;
+        const editableTodo = req.body.id;
+        console.log("EDITABLE"+editableTodo);
         
-        todos[editableTodo].action = req.body.action;
         
-        res.redirect('/');
+
+
+        for (let i = 0; i<todos.length; i++){
+            if(todos[i].id==editableTodo){
+                console.log("ird is "+ i);
+                todos[i].action = req.body.action;
+                console.log(todos[i]);
+            }
+    
+        }
+        res.send("editable todo was ");
 
         //console.log(todos.editableTodo.action)
 
     
-    res.end();
+    //res.end();
 
 
 });
 
 
-app.get('/del/:id', (req,res)=>{
 
-    const deletableTodo = req.params.id;
 
-    todos.splice(deletableTodo,1);
 
-    res.redirect('/');
+app.delete('/api/del', (req,res)=>{
+
+    //console.log('inside of delete request');
+    
+
+
+
+    const deletableTodo = Number(req.body.id);
+
+
+    console.log("deletabletodo is "+deletableTodo);
+    for (let i = 0; i<todos.length; i++){
+        if(todos[i].id==deletableTodo){
+            console.log(i);
+
+            todos.splice(i , 1);
+        }
+
+    }
+    
+    
+
+    
+
+    
+    console.log("todos after"+todos);
+    res.send({ id: req.body.id});
+    //res.redirect('/');
+
 
 })
+
+
 
 
 
